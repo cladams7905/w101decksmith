9844af7b8c077e6caafe1a1b4b39d83c5d532f27
Carter Adams <cladams7905@gmail.com>
2025-06-03 20:20:40 -0600

    Change favicon

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function HomePage() {
-  return <div>My Decks</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gradient-special">
+      <h1 className="text-white text-4xl font-bold">My Decks</h1>
+    </div>
+  );
 }
