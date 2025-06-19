4233cd5d14a9f838f3242cbc273da7c3b2371ce8
Carter Adams <cladams7905@gmail.com>
2025-06-18 21:17:14 -0600

    Trying to get cron deploy to trigger

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gradient-special">
-      <h1 className="text-white text-4xl font-bold">My Decks</h1>
+      <h1 className="text-white text-4xl font-bold">My Decks (Coming Soon)</h1>
     </div>
   );
 }
